64bdc1c1e6d54b9875d70d45af2d40baac715c4f
Kate Negrienko <katya@negrienko.com>
2018-07-11 15:53:27 +0300

    little update

--- src/drivers/index.js
+++ src/drivers/index.js
@@ -1,6 +1,5 @@
 import BtcDotCom from './BtcDotCom.driver'
 import BlockCypher from './BlockCypher.driver'
-// import Insight from './insight'
 import Driver from './Driver'
-//
+
 export default { BlockCypher, BtcDotCom, Driver }
